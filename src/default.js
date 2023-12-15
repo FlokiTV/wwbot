76e22452511e0edfb0f336afacaae204c3eaa1b8
Nyco3D <nweissberg@hotmail.com>
2023-12-15 10:29:42 -0300

    New commands for bot

--- src/default.js
+++ src/default.js
@@ -1,3 +1,35 @@
+const body = `Assistente
+Bem vindo, o que quer fazer?
+{balance.balance}
+1 - ajuda
+2 - teste
+3 - Hugging Face`;
+
 module.exports = cmd => {
-    cmd.body = `Hello World`
+    cmd.arguments({
+        value: "number",
+        dia: "string"
+    })
+
+    cmd.request("huggingFace", {
+        method: "post",
+        url: "https://api-inference.huggingface.co",
+        headers: { Authorization: "Bearer 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" },
+        body: JSON.stringify({
+            inputs: "Can you please let us know more details about your ",
+            parameters: {
+                model: "gpt2",
+            }
+        })
+    })
+
+    cmd.request("balance",{
+        method: "get",
+        url: "https://verifiedscatacado.com/api/v2?key=d86e470375f56782f7cdc28dd04fc5a2&action=balance"
+    })
+    cmd.body = body
+
+    cmd.callback("1", "ajuda")
+    cmd.callback("2", "teste")
+    cmd.callback("3", "huggingFace")
 }
--- src/teste.um.js
+++
@@ -1,3 +0,0 @@
-module.exports = cmd => {
-    cmd.body = `Hello World`
-}
